de8d709450f0cb268820fdb90f594a0659575382
Mr.SAY SEAK LENG <Mr.SAY SEAK LENG@192.168.1.101>
2016-07-31 21:57:02 +0700

    Update Thymeleaf version

--- spring-boot/src/main/java/com/kdemo/springboot/config/EmbeddedServletContainerConfiguration.java
+++ spring-boot/src/main/java/com/kdemo/springboot/config/EmbeddedServletContainerConfiguration.java
@@ -1,10 +1,20 @@
 package com.kdemo.springboot.config;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.PortInUseException;
 import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
 import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 import io.undertow.Undertow.Builder;
 import io.undertow.UndertowOptions;
@@ -27,5 +37,43 @@ public class EmbeddedServletContainerConfiguration {
 	            });
 	    
 	    return undertowEmbeddedServletContainerFactory;
+	}
+	
+	/**
+	 * To add application listener
+	 * <br/>
+	 * If application's successfully started, then browser will launch application default home page
+	 * @param context
+	 */
+	@Autowired(required = true)
+	public void configure(ConfigurableApplicationContext context) {
+		context.addApplicationListener( event-> {
+			if (event instanceof ApplicationFailedEvent) {
+				if(((ApplicationFailedEvent) event).getException() instanceof PortInUseException) {
+					// System.exit(1);
+				}
+			}
+			else if(event instanceof ContextRefreshedEvent) {
+				
+				String url = "https://localhost:8443/page";
+				
+				// check if it can open default browser
+				if(Desktop.isDesktopSupported()){
+		            Desktop desktop = Desktop.getDesktop();
+		            try {
+		                desktop.browse(new URI(url));
+		            } catch (IOException | URISyntaxException e) {
+		                e.printStackTrace();
+		            }
+		        }else{
+		            Runtime runtime = Runtime.getRuntime();
+		            try {
+		                runtime.exec(new String[]{"cmd", "/c","start chrome " + url});
+		            } catch (IOException e) {
+		                e.printStackTrace();
+		            }
+		        }
+			}
+		});
 	}
 }
